cbdad90772a494c197f3ff5ccfcd58bed7a5b853
Howard Ding <hding2@illinois.edu>
2012-11-30 10:33:56 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DxBitIngest::Application.config.secret_token = 'c1372c1cfe0c0c406fc3375a49120cdab6e501fc4a1bfb84f9a5c6f5cf538b40f41294e841b5d1e4e17bcb1355c19276616d171b722517bd0588442d534a0cee'
