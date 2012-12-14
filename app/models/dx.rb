46a15634337336e002c6e4e84cc15e7d75e4be5a
Howard Ding <hding2@illinois.edu>
2012-12-14 16:18:56 -0600

    Refactor retry stuff into its own control structure.

--- app/models/dx.rb
+++ app/models/dx.rb
@@ -9,66 +9,61 @@ class Dx < Object
     self.configure
   end
 
-  def ingest_file(file_path, bit_file, opts = {})
-    opts[:retries] ||= 5
-    content = File.open(file_path, 'rb') { |f| f.read }
-    self.client.post(file_url(bit_file), content, ingest_headers(bit_file, file_path, opts))
-    Rails.logger.info "DX Ingested #{bit_file.name}"
+  def with_retries(opts, success_message, error_message, retry_function, *retry_arguments)
+    yield
+    Rails.logger.info success_message
   rescue Exception => e
-    Rails.logger.error "Error DX Ingesting #{bit_file.name}: #{e}"
+    Rails.logger.error "#{error_message}: #{e}"
     if opts[:retries] == 0
       Rails.logger.error "Aborting."
       raise e
     else
-      Rails.logger.info "Retrying."
-      ingest_file(file_path, bit_file, opts.merge(:retries => opts[:retries] - 1))
+      Rails.logger.error "Retrying. #{opts[:retries]} remaining."
+      self.send(retry_function, *retry_arguments)
+    end
+  end
+
+  def ingest_file(file_path, bit_file, opts = {})
+    opts[:retries] ||= 5
+    with_retries(opts, "DX Ingested #{bit_file.name}", "Error DX Ingesting #{bit_file.name}", :ingest_file, file_path, bit_file, opts.merge(:retries => opts[:retries] - 1)) do
+      content = File.open(file_path, 'rb') { |f| f.read }
+      self.client.post(file_url(bit_file), content, ingest_headers(bit_file, file_path, opts))
     end
   end
 
   def delete_file(bit_file, retries = 5)
-    begin
-      Rails.logger.info("Trying to delete #{file_url(bit_file)} with #{delete_headers(bit_file)}")
-      response = self.client.delete(file_url(bit_file), {}, delete_headers(bit_file))
-      Rails.logger.info("DX Deleted #{bit_file.name}. HTTP Code: #{response.code}")
-    rescue Mechanize::ResponseCodeError => e
-      if e.response_code.to_i == 403 or e.response_code.to_i == 404
-        Rails.logger.info "#{bit_file.name} already deleted from DX."
-      else
-        raise e
+    with_retries({:retries => retries}, "DX Deleted #{bit_file.name}:#{bit_file.dx_name}.", "Error DX Deleting #{bit_file.name}:#{bit_file.dx_name}", :delete_file, bit_file, retries - 1) do
+      begin
+        self.client.delete(file_url(bit_file), {}, delete_headers(bit_file))
+      rescue Mechanize::ResponseCodeError => e
+        if e.response_code.to_i == 403 or e.response_code.to_i == 404
+          Rails.logger.info "#{bit_file.name} already deleted from DX."
+        else
+          raise e
+        end
       end
-    end
-  rescue Exception => e
-    Rails.logger.error "Error DX Deleting #{bit_file.name}: #{e}"
-    if retries == 0
-      Rails.logger.error "Aborting."
-      raise e
-    else
-      Rails.logger.info "Retrying."
-      delete_file(bit_file, retries - 1)
     end
   end
 
   def export_file(bit_file, target_directory, retries = 5)
     filename = File.join(target_directory, bit_file.name)
-    response = self.client.download(file_url(bit_file), filename, [], nil, export_headers(bit_file))
-    begin
-      atime = Time.parse(response.header['x-bit-meta-atime'])
-      mtime = Time.parse(response.header['x-bit-meta-mtime'])
-      File.utime(atime, mtime, bit_file.name)
-    rescue Exception => e
-      Rails.logger.error "Problem resetting atime and mtime for #{filename}. Skipping"
-    end
-    Rails.logger.info("DX exported file: #{bit_file.name}")
-  rescue Exception => e
-    Rails.logger.error "Error DX exporting #{bit_file.name}: #{e}"
-    if retries == 0
-      Rails.logger.error 'Aborting.'
-      raise e
-    else
-      Rails.logger.info "Retrying."
-      export_file(bit_file, target_directory, retries - 1)
+    with_retries({:retries => retries}, "DX exported file: #{bit_file.name}", "Error DX exporting #{bit_file.name}", :export_file, bit_file, target_directory, retries - 1) do
+      response = self.client.download(file_url(bit_file), filename, [], nil, export_headers(bit_file))
+      begin
+        atime = Time.parse(response.header['x-bit-meta-atime'])
+        mtime = Time.parse(response.header['x-bit-meta-mtime'])
+        File.utime(atime, mtime, bit_file.name)
+      rescue Exception => e
+        Rails.logger.error "Problem resetting atime and mtime for #{filename}. Skipping"
+      end
     end
   end
+
+  def file_url(bit_file)
+    "http://#{self.entry_host}/#{self.bucket}/#{bit_file.dx_name}"
+  end
+
+  protected
 
   def configure
     config = YAML.load_file(File.join(Rails.root, 'config', 'dx.yml'))[Rails.env]
@@ -87,13 +82,9 @@ class Dx < Object
     self.use_test_headers = config['use_test_headers'] || false
   end
 
-  def file_url(bit_file)
-    "http://#{self.entry_host}/#{self.bucket}/#{bit_file.dx_name}"
-  end
-
   def ingest_headers(bit_file, file_path, opts)
     Hash.new.tap do |headers|
-      headers['Host'] = self.domain if self.domain
+      add_domain_header(headers)
       headers['Content-Type'] = bit_file.content_type || 'application/octet-stream'
       headers['Content-MD5'] = bit_file.md5sum if bit_file.md5sum
       headers['x-bit-meta-atime'] = File.atime(file_path).to_s
@@ -101,25 +92,33 @@ class Dx < Object
       headers['x-bit-meta-mtime'] = File.mtime(file_path).to_s
       headers['x-bit-meta-path'] = File.join(opts[:path], bit_file.name)
       headers['x-bit-meta-root-id'] = opts[:root_id]
-      if self.use_test_headers
-        #set lifepoint to assure that content gets deleted after 2 weeks even if we don't clean it up manually
-        headers['Lifepoint'] = ["[#{(Time.now + 2.weeks).httpdate}] reps=2, deletable=yes",
-                                "[] delete"]
-      else
-        headers['Lifepoint'] = "[] reps=3, deletable=yes"
-      end
+      add_lifepoint_header(headers)
     end
   end
 
   def delete_headers(bit_file)
     Hash.new.tap do |headers|
-      headers['Host'] = self.domain if self.domain
+      add_domain_header(headers)
     end
   end
 
   def export_headers(bit_file)
     Hash.new.tap do |headers|
-      headers['Host'] = self.domain if self.domain
+      add_domain_header(headers)
+    end
+  end
+
+  def add_domain_header(headers)
+    headers['Host'] = self.domain if self.domain
+  end
+
+  def add_lifepoint_header(headers)
+    if self.use_test_headers
+      #set lifepoint to assure that content gets deleted after 2 weeks even if we don't clean it up manually
+      headers['Lifepoint'] = ["[#{(Time.now + 2.weeks).httpdate}] reps=2, deletable=yes",
+                              "[] delete"]
+    else
+      headers['Lifepoint'] = "[] reps=3, deletable=yes"
     end
   end
 
